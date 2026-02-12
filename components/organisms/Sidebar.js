f2047de22fbc3a3db37ce0fa3f59eb6cd7df83a0
mysite1234 <mano321114@gmail.com>
2026-02-12 22:35:40 +0530

    Latest Code

--- components/organisms/Sidebar.js
+++ components/organisms/Sidebar.js
@@ -37,21 +37,7 @@ const navigationItems = [
     label: 'Dashboard', 
     icon: Home 
   },
-  { 
-    href: '/customers', 
-    label: 'Event Registration', 
-    icon: CalendarCheck 
-  },
-  { 
-    href: '/residency-details', 
-    label: 'Residency Details', 
-    icon: Building2 
-  },
-  { 
-    href: '/events-type', 
-    label: 'Events Type', 
-    icon: Building2 
-  },
+  
 /*   { 
     href: '/users', 
     label: 'Users', 
